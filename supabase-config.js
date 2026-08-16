9e85d8480515fab8f62036bb4b33403fb3b74e1a
raphaelbuenocaptacao-creator <raphaelbueno.captacao@gmail.com>
2026-08-16 09:09:29 -0300

    fix: corrige URL e anon key do Supabase

--- supabase-config.js
+++ supabase-config.js
@@ -2,5 +2,5 @@
 // Chave pública anon. NUNCA coloque service_role no frontend.
 window.SUPABASE_CONFIG = {
   url: 'https://xiejbwdvwjnfhlcnxhen.supabase.co',
-  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhpZWpid2R2eWpuZmhsY25oZXJ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY4NTk1NjUsImV4cCI6MjEwMjQzNTU2NX0._n9lTfSfdDfX3ZbziNlzvK4Hh3QkNLkOPrPZ8xor27I'
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhpZWpid2R2d2puZmhsY25oZW4iLCJyb2xlIjoiYW5vbiIsImlhdCI6MTc4Njg1OTU2NSwiZXhwIjoyMTAyNDM1NTY1fQ._n9lTfSfdDfX3ZbziNlzvK4Hh3QkNLkOPrPZ8xor27I'
 };

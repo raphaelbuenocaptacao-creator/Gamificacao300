f029851736b053381adcb06c5497cd5c53cbc493
raphaelbuenocaptacao-creator <raphaelbueno.captacao@gmail.com>
2026-08-16 09:11:54 -0300

    feat: carrega controles administrativos da Arena

--- supabase-config.js
+++ supabase-config.js
@@ -4,3 +4,5 @@ window.SUPABASE_CONFIG = {
   url: 'https://xiejbwdvwjnfhlcnxhen.supabase.co',
   anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhpZWpid2R2d2puZmhsY25oZW4iLCJyb2xlIjoiYW5vbiIsImlhdCI6MTc4Njg1OTU2NSwiZXhwIjoyMTAyNDM1NTY1fQ._n9lTfSfdDfX3ZbziNlzvK4Hh3QkNLkOPrPZ8xor27I'
 };
+// Injeta o painel administrativo antes do motor Arena XP.
+document.write('<script src="admin-controls.js"><\\/script>');
